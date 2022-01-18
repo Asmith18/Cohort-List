8e272410d5494a329cbaa9dacd81d2719bcab329
Asmith18 <Smittysadam@gmail.com>
2022-01-18 17:55:25 -0600

    completed project

--- CohortList/CohortList/View Controllers/StudentDetailViewController.swift
+++ CohortList/CohortList/View Controllers/StudentDetailViewController.swift
@@ -7,34 +7,55 @@
 
 import UIKit
 
+//MARK: - Outlets
 class StudentDetailViewController: UIViewController {
     @IBOutlet weak var studentDetails: UITextView!
-    @IBOutlet weak var cohortLabel: UILabel!
     @IBOutlet weak var cohortNumText: UITextField!
     @IBOutlet weak var studentNameText: UITextField!
     
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews(student: studentReceiver)
     }
-    @IBAction func deleteBtn(_ sender: UIButton) {
+    //MARK: - Properties
+    var studentReceiver: Student? = nil
+    
+    //MARK: - Helper Methods
+    func updateViews(student: Student?) {
+        guard let student = student else { return }
+        studentNameText.text = student.name
+        cohortNumText.text = String(student.cohortNumber)
+        studentDetails.text = student.studentDescription
     }
     
+    func clearData() {
+        studentDetails.text = ""
+        cohortNumText.text = ""
+        studentNameText.text = ""
+    }
+    //MARK: - Actions
+    @IBAction func deleteBtn(_ sender: UIButton) {
+        if let studentReceiver = studentReceiver {
+            StudentController.sharedInstance.deleteStudent(student: studentReceiver)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
     @IBAction func clearBtn(_ sender: UIButton) {
+        clearData()
     }
     @IBAction func saveBtn(_ sender: UIBarButtonItem) {
+        guard let name = studentNameText.text, let cohortNum = cohortNumText.text, let studentDetails = studentDetails.text, let studentCohortNumber = Int (cohortNum) else { return }
+        
+        
+        if let studentReceiver = studentReceiver {
+            // has a value. the user wants to update
+            StudentController.sharedInstance.updateStudent(student: studentReceiver, newName: name, newCohortNum: studentCohortNumber, newDescription: studentDetails)
+            
+        } else {
+            //does not have a value. the user wants to do create
+            StudentController.sharedInstance.createStudent(name: name, cohortNumber: studentCohortNumber, studentDescription: studentDetails)
+        }
+        self.navigationController?.popViewController(animated: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}//MARK: - End of Class
