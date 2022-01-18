255df354a753f1227846545efc906bec0e5b95cd
Asmith18 <Smittysadam@gmail.com>
2022-01-18 15:53:01 -0600

    updated model and CRUD functions

--- CohortList/CohortList/Model Controllers/StudentController.swift
+++ CohortList/CohortList/Model Controllers/StudentController.swift
@@ -15,13 +15,20 @@ class StudentController {
 
     
     //MARK: CRUD Functions
-    func createStudent(name: String, age: String) { //Can add other parameters here for second text input
-        let newStudent: Student = Student(name: name, age: age, favoriteColor: "Red")
+    func createStudent(name: String, cohortNumber: Int, studentDescription: String) { //Can add other parameters here for second text input
+        let newStudent: Student = Student(name: name, cohortNumber: cohortNumber, studentDescription: "Red")
         studentList.append(newStudent)
     }
     
     func deleteStudent(student: Student) {
         guard let index = studentList.firstIndex(where: {$0 == student}) else {return}
         studentList.remove(at: index)
+    }
+    
+    func updateStudent(student: Student, newName: String, newCohortNum: Int, newDescription: String) {
+        student.name = newName
+        student.cohortNumber = newCohortNum
+        student.studentDescription = newDescription
+        
     }
 }
--- CohortList/CohortList/View Controllers/StudentListTableViewController.swift
+++ CohortList/CohortList/View Controllers/StudentListTableViewController.swift
@@ -8,82 +8,83 @@
 import UIKit
 
 class StudentListTableViewController: UITableViewController, UITextFieldDelegate{
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return StudentController.sharedInstance.studentList.count // 5
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         let studentList = StudentController.sharedInstance.studentList
         let studentForIndexPath = studentList[indexPath.row] // 0 // 1
-  
+        
         cell.textLabel?.text = studentForIndexPath.name
-        cell.textLabel?.text?.append(": \(studentList[indexPath.row].age)")
-    
+        cell.textLabel?.text?.append(": \(studentList[indexPath.row].cohortNumber)")
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            let student = Student(name: StudentController.sharedInstance.studentList[indexPath.row].name,
-                                  age: StudentController.sharedInstance.studentList[indexPath.row].age,
-                                  favoriteColor: StudentController.sharedInstance.studentList[indexPath.row].favoriteColor)
+            //            // Delete the row from the data source
+            //            let student = Student(name: StudentController.sharedInstance.studentList[indexPath.row].name,
+            //                                  age: StudentController.sharedInstance.studentList[indexPath.row].cohortNumber,
+            //                                  favoriteColor: StudentController.sharedInstance.studentList[indexPath.row].studentDescription)
+            //
+            //            StudentController.sharedInstance.deleteStudent(student: student)
+            //
+            //            tableView.deleteRows(at: [indexPath], with: .fade)
+            //        }
+            //    }
             
-            StudentController.sharedInstance.deleteStudent(student: student)
             
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } 
+            /*
+             // Override to support rearranging the table view.
+             override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+             
+             }
+             */
+            
+            /*
+             // Override to support conditional rearranging of the table view.
+             override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+             // Return false if you do not want the item to be re-orderable.
+             return true
+             }
+             */
+            
+            /*
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
+        }
     }
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
